6159cab7ab647d50dad916763bc5f9030177958a
mishalturkane <mishalturkane@gmail.com>
2025-12-13 01:28:24 +0530

    token-create added

--- create-token/tests/create-token.ts
+++ create-token/tests/create-token.ts
@@ -1,16 +1,54 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { CreateToken } from "../target/types/create_token";
+import * as anchor from '@coral-xyz/anchor';
+import { Keypair } from '@solana/web3.js';
+import type { CreateToken } from '../target/types/create_token';
 
-describe("create-token", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('Create Tokens', () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const payer = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.CreateToken as anchor.Program<CreateToken>;
 
-  const program = anchor.workspace.createToken as Program<CreateToken>;
+  const metadata = {
+    name: 'Solana Gold',
+    symbol: 'GOLDSOL',
+    uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+  };
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('Create an SPL Token!', async () => {
+    // Generate new keypair to use as address for mint account.
+    const mintKeypair = new Keypair();
+
+    // SPL Token default = 9 decimals
+    const transactionSignature = await program.methods
+      .createTokenMint(9, metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      })
+      .signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
+  });
+
+  it('Create an NFT!', async () => {
+    // Generate new keypair to use as address for mint account.
+    const mintKeypair = new Keypair();
+
+    // NFT default = 0 decimals
+    const transactionSignature = await program.methods
+      .createTokenMint(0, metadata.name, metadata.symbol, metadata.uri)
+      .accounts({
+        payer: payer.publicKey,
+        mintAccount: mintKeypair.publicKey,
+      })
+      .signers([mintKeypair])
+      .rpc();
+
+    console.log('Success!');
+    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Transaction Signature: ${transactionSignature}`);
   });
 });
