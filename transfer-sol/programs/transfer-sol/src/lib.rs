3dfd2853074df0072794c996d400cd6fa41a1bb6
mishalturkane <mishalturkane@gmail.com>
2026-01-23 08:30:31 +0530

    code modified

--- transfer-sol/programs/transfer-sol/src/lib.rs
+++ transfer-sol/programs/transfer-sol/src/lib.rs
@@ -21,16 +21,7 @@ pub mod transfer_sol {
 
         Ok(())
     }
-
-    // Directly modifying lamports is only possible if the program is the owner of the account
-    pub fn transfer_sol_with_program(
-        ctx: Context<TransferSolWithProgram>,
-        amount: u64,
-    ) -> Result<()> {
-        **ctx.accounts.payer.try_borrow_mut_lamports()? -= amount;
-        **ctx.accounts.recipient.try_borrow_mut_lamports()? += amount;
-        Ok(())
-    }
+    
 }
 
 #[derive(Accounts)]
@@ -40,16 +31,4 @@ pub struct TransferSolWithCpi<'info> {
     #[account(mut)]
     recipient: SystemAccount<'info>,
     system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct TransferSolWithProgram<'info> {
-    /// CHECK: Use owner constraint to check account is owned by our program
-    #[account(
-        mut,
-        owner = id() // value of declare_id!()
-    )]
-    payer: UncheckedAccount<'info>,
-    #[account(mut)]
-    recipient: SystemAccount<'info>,
 }
